9fbbb1b9c88b770d4277bbb054d9242d380c758c
Agrard <gabor.kallai12@gmail.com>
2023-02-22 15:56:41 +0100

    Bootstrap fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormComponent } from './components/form/form.component';
+import { NgbModule,NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { LayoutComponent } from './components/layout/layout.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
     AppRoutingModule
   ],
   providers: [],
